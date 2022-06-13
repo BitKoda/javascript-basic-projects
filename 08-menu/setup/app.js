8fead8dc29a10b15e65435ff8a3cb0e0ceee72ea
Jamie Finlay <jpfinlay@gmail.com>
2022-06-13 18:13:31 +0100

    Complete diner menu project

--- 08-menu/setup/app.js
+++ 08-menu/setup/app.js
@@ -71,4 +71,77 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 29.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
+
+const sectionCenter = document.querySelector('.section-center');
+const btnContainer = document.querySelector('.btn-container');
+
+// load menu items
+window.addEventListener('DOMContentLoaded', function () {
+  displayMenuButtons();
+  displayMenuItems(menu);
+});
+
+
+function displayMenuItems(menuItems) {
+  let mappedMenu = menuItems.map(function (item) {
+
+    return `<article class="menu-item">
+    <img src="${item.img}" class="photo" alt="${item.title}">
+    <div class="item-info">
+    <header>
+    <h4>${item.title}</h4>
+    <h4 class="price">$${item.price}</h4>
+    </header>
+    <p class="item-text">
+    ${item.desc}
+    </p>
+    </div>
+    </article>`;
+  });
+  mappedMenu = mappedMenu.join("");
+  sectionCenter.innerHTML = mappedMenu;
+}
+
+function displayMenuButtons() {
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category)
+      }
+      return values;
+    }, ["all"]
+  );
+
+  const categoryBtns = categories.map(function (category) {
+    return `<button class="filter-btn" type="button" data-category="${category}">${category}</button>`
+  }).join("");
+  btnContainer.innerHTML = categoryBtns;
+  const filterBtns = document.querySelectorAll('.filter-btn');
+
+  // filter menu items
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+      console.log(e)
+      const category = e.currentTarget.dataset.category;
+      const menuCategory = menu.filter(function (menuItem) {
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === "all") {
+        displayMenuItems(menu)
+      } else {
+        displayMenuItems(menuCategory)
+      }
+    });
+  });
+}
