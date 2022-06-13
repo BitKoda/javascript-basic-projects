56a13b3a4a4e4a54cabe734a7128782c332967f2
Jamie Finlay <jpfinlay@gmail.com>
2022-06-13 16:04:40 +0100

    Complete questions display with toggle project

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,25 @@
-//using selectors inside the element
-// traversing the dom
+// traversing the DOM
+// const qBtns = document.querySelectorAll('.question-btn');
+
+// qBtns.forEach(function (btn) {
+//     btn.addEventListener('click', function(e) {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     })
+// })
+
+// using selectors inside element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function (question) {
+    const btn = question.querySelector('.question-btn');
+    
+    btn.addEventListener('click', function () {
+        questions.forEach(function (others) {
+            if (others !== question) { others.classList.remove('show-text') };
+        });
+        
+        question.classList.toggle('show-text');
+    });
+});
+
